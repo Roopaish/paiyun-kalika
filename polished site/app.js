0327e3a116ba6f22e0921cceb33d08ff5795db8f
roopaish <54392112+Roopaish@users.noreply.github.com>
2021-10-22 14:44:02 +0545

    navbar fixed

---
+++ polished site/app.js
@@ -0,0 +1,19 @@
+const header = document.querySelector(".header");
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 150) {
+    header.style.backgroundColor = "white";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+const toggler = document.querySelector("#toggle-navbar");
+
+toggler.addEventListener("change", () => {
+  if (toggler.checked) {
+    header.style.backgroundColor = "white";
+  }else{
+    header.style.backgroundColor = "transparent";
+  }
+});
