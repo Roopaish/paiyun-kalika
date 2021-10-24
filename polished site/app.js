b6d57acede3e4507099b47b0aedef2f86e1e3438
roopaish <54392112+Roopaish@users.noreply.github.com>
2021-10-24 11:45:23 +0545

    fb chat added

--- polished site/app.js
+++ polished site/app.js
@@ -13,21 +13,7 @@ const toggler = document.querySelector("#toggle-navbar");
 toggler.addEventListener("change", () => {
   if (toggler.checked) {
     header.style.backgroundColor = "white";
-  }else{
+  } else {
     header.style.backgroundColor = "transparent";
   }
-});
-
-// mansonry
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
-
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  
 });
